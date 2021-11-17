86ae8c5fbdffc520530a16282ca39e9a12634c37
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2021-11-17 20:08:49 +0100

    init components

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>November, 17th 2021</div>
+      <div>
+        <h2>JBL Box</h2>
+        <div>$149,00</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
