e9daa5f1198e45b78a6fb90bcaa9e3de319aea27
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2021-11-20 21:41:24 +0100

    Expenses Chart animated

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,25 @@
+import "./Chart.css";
+
+import ChartBar from "./ChartBar";
+
+const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const maxDataPointValue = Math.max(...dataPointValues);
+
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => {
+        return (
+          <ChartBar
+            key={dataPoint.label}
+            value={dataPoint.value}
+            maxValue={maxDataPointValue}
+            label={dataPoint.label}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Chart;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filterYear, setFilterYear] = useState("2021");
@@ -18,6 +19,7 @@ const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filterYear} onFilter={addFilterHandler} />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList expenses={filteredExpenses} />
     </Card>
   );
